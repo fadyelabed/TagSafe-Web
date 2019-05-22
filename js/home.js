303258d7da6e744fc659360569bc73a8d7053e74
El Baba Mayes <mayes.el.baba@student.ehb.be>
2019-05-22 15:49:04 +0200

    8 Tags getoont

--- js/home.js
+++ js/home.js
@@ -1,17 +1,45 @@
+// tags tonen in de homepagina
 $(function () {
     var db = firebase.firestore();
-    
-    var section = $(".tags")
 
-    db.collection("tags").get().then(function (querySnapshot) {
+    var section = $(".tags")
+    var tagsRef = db.collection("tags");
+    
+    var uid;
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            console.log("successfully logged in");
+            console.log(user.uid);
+            uid = user.uid
+
+            db.collection("users").doc(uid).get().then(function (doc) {
+                if (doc.exists) {
+                    console.log("Document data:", doc.data());
+                } else {
+                    // doc.data() will be undefined in this case
+                    console.log("No such document!");
+                }
+            }).catch(function (error) {
+                console.log("Error getting document:", error);
+            });
+
+        } else {
+            console.log("no success");
+
+        }
+    });
+
+
+    db.collection("tags").orderBy("name").limit(8).get().then(function (querySnapshot) {
         querySnapshot.forEach(function (doc) {
-       
+
             // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
+            //console.log(doc.id, " => ", doc.data());
             var data = doc.data()
             console.log(data["name"]);
-            
-            section.append('<a href="#"><span>'+ data["name"] + '</span></a>')
+
+            section.append('<a href="#"><p>' + data["name"] + " " + '</p></a>')
         });
     });
 });
@@ -22,67 +50,42 @@ function logout(){
     console.log(logout);
     window.location.href = "../index.html";
 
-    
-    // tags tonen in de homepagina
-    
-//    
-//    ref.on("value", function(snapshot) {
-//    for (x in snapshot.val()) {
-//        var xRef = new Firebase("https://tagsafe-e1bf4.firebaseio.com");
-//        xRef.once("value", function(xsnapshot) {
-//            var data = xsnapshot.val();
-//            var name = data["tags"];
-//            console.log(name);
-//        });
-//    }
-//});
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-//    function getTags(){
-//        $.ajax({
-//           databaseURL: "https://tagsafe-e1bf4.firebaseio.com",
-//            methode:'GET',
-//            data:{
-//                   apiKey: "AIzaSyD3-3yCC41HWr-AJYdXkPDSfxD6QEFii5Y",
-//            authDomain: "tagsafe-e1bf4.firebaseapp.com",
-////            databaseURL: "https://tagsafe-e1bf4.firebaseio.com",
-//            projectId: "tagsafe-e1bf4",
-//            storageBucket: "tagsafe-e1bf4.appspot.com",
-//            messagingSenderId: "719548532308",
-//            appId: "1:719548532308:web:ff7914623e41b34b"
-//            }
-//            
-//        }).done(function (data) {
-//		        printTags(data);
-//		    }).fail(function (error) {
-//		        console.log('Fail');
-//		    });
-//    };
-    
-   
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 };
+
+
+
+// tags tonen in de homepagina
+//$(function () {
+//    var db = firebase.firestore();
+//
+//    var section = $(".tags")
+//    var tagsRef = db.collection("tags");
+//
+//    //    var tags = tagsRef.orderBy("name").limit(3);
+//    //    console.log(tags);
+//
+//    var uid;
+//
+//    firebase.auth().onAuthStateChanged(function (user) {
+//        if (user) {
+//            console.log("successfully logged in");
+//            console.log(user.uid);
+//            uid = user.uid
+//
+//            db.collection("users").doc(uid).get().then(function (doc) {
+//                if (doc.exists) {
+//                    console.log("Document data:", doc.data());
+//                } else {
+//                    // doc.data() will be undefined in this case
+//                    console.log("No such document!");
+//                }
+//            }).catch(function (error) {
+//                console.log("Error getting document:", error);
+//            });
+//
+//        } else {
+//            console.log("no success");
+//
+//        }
+//    });
+//
