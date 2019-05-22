2fca07bdb9bfdca5fdee3f8a1aacc3ac0e615c6e
El Baba Mayes <mayes.el.baba@student.ehb.be>
2019-05-22 10:17:58 +0200

    html voor firebase aangepast

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+console.log("Js loaded!");
+
+function login() {
+    var userEmail = document.getElementById("email").value;
+    var userPassword = document.getElementById("password").value;
+    //console.log(userEmail + " " + userPassword);
+    //  window.alert(userEmail + " " + userPassword);
+
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorMessage);
+    });
+    
+        
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        console.log("successfully logged in");
+        window.location.href = "pages/home.html";
+        console.log(user);
+
+    } else {
+        console.log("no success");
+
+    }
+});
+
+};
+
 $(function () {
     $(".logonav").mouseenter(function () {
         $(this).attr('src', "../images/mouseenter.gif");
